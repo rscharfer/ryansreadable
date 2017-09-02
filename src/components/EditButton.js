6ca7640b41f9b0f5df99df67a5c4e39dddd7e037
Ryan <rscharfer@gmail.com>
2017-09-02 06:21:28 +0200

    initial commit

---
+++ src/components/EditButton.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+
+
+ class EditButton extends React.Component {
+
+
+	render(){
+
+		return     (<button className="button is-success">
+      Login
+    </button>)
+	}}
+
+
+
+export default EditButton
+
+
+
+
