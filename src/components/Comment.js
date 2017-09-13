a7208e5b61bf0e35a3de639e4f717bcc4fb5ba82
Ryan Scharfer <rscharfer@gmail.com>
2017-09-13 12:55:13 +0200

    commment is now stateful

--- src/components/Comment.js
+++ src/components/Comment.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { Component } from 'react'
 import dateFormat from 'dateformat'
 import VoteUp from './buttons/VoteUp.js'
 import VoteDown from './buttons/VoteDown.js'
@@ -7,57 +7,57 @@ import Delete from './buttons/Delete.js'
 
 
 
-function Comment(props){
+class Comment extends Component{
 
 
+  constructor(props){
+    super()
+  }
+
+  render(){
+
+    const time = dateFormat(this.props.comment.timestamp, "mmmm dS, yyyy, h:MM:ss TT")
+
+    return (    
+          <div className="columns is-centered">
+            <div className="column is-10">
+              <article className="media">
+              <figure className="media-left">
+                <p className="image is-64x64">
+                  <img src={"http://bulma.io/images/placeholders/128x128.png"}/>
+                </p>
+              </figure>
+              <div className="media-content">
+                <div className="content">
+                  <p>
+                    <strong>{this.props.comment.author}</strong> <small>{time}</small><small>&nbsp;&nbsp;Total votes: {this.props.comment.voteScore}</small>
+                    <br/>
+                    {this.props.comment.body}
+                  </p>
+               </div>
+            </div>
+            <div className="media-right">
+              <nav className="breadcrumb" aria-label="breadcrumbs">
+                  <ul>
+                    <Edit meta={this.props.comment} comment={true}  changeEditId={this.props.changeEditId} showPopulatedCommentForm={this.props.showPopulatedCommentForm}/>
+                    <Delete meta={this.props.comment} comment={true} removeComment={this.props.removeComment}/>  
+                    <VoteUp meta={this.props.comment} changeVote={this.changeVoteTotal}/>
+                    <VoteDown meta={this.props.comment} changeVote={this.changeVoteTotal}/>
+                  </ul>
+              </nav>
+            </div>
+            </article>
+          </div>
+        </div>
+        )
+  }
  
-  const time = dateFormat(props.comment.timestamp, "mmmm dS, yyyy, h:MM:ss TT")
+  
+
+
 
   // props 'comment' contains author, body, deleted, id, parentDeleted, parentID, timestamp, votescore
 
-    return (
-
-    
-      <div className="columns is-centered">
-      <div className="column is-10">
-        <article className="media">
-  <figure className="media-left">
-    <p className="image is-64x64">
-      <img src={"http://bulma.io/images/placeholders/128x128.png"}/>
-    </p>
-  </figure>
-  <div className="media-content">
-    <div className="content">
-      <p>
-        <strong>{props.comment.author}</strong> <small>{time}</small><small>&nbsp;&nbsp;Total votes: {props.comment.voteScore}</small>
-        <br/>
-        {props.comment.body}
-      </p>
-    </div>
-
-  </div>
-  <div className="media-right">
-     <nav className="breadcrumb" aria-label="breadcrumbs">
-                <ul>
-               
-                  <Edit meta={props.comment} comment={true}  changeEditId={props.changeEditId} showPopulatedCommentForm={props.showPopulatedCommentForm}/>
-                  <Delete meta={props.comment} comment={true} removeComment={props.removeComment}/>  
-                  <li><a href="#"><span className="icon is-small"><i className="fa fa-thumbs-up"></i></span><span>Vote Up</span></a></li>
-                  <li><a href="#"><span className="icon is-small"><i className="fa fa-thumbs-down"></i></span><span>Vote Down</span></a></li>
-                  
-              </ul>
-            </nav>
-  </div>
-</article>
-</div>
-</div>
-
-      
-      
-
-
-    
-    )
 }
 
 
