042335c91575c746c148e047451923ca6597b44e
Ryan <rscharfer@gmail.com>
2017-09-24 10:13:37 +0200

    comment reducer

--- src/reducers/commentReducer.js
+++ src/reducers/commentReducer.js
@@ -1,59 +1,46 @@
-const SAVEEDITEDPOST = "SAVEEDITEDPOST";
-const SAVENEWPOST = "SAVENEWPOST";
 const SAVENEWCOMMENT = "SAVENEWCOMMENT";
 const SAVEEDITEDCOMMENT = "SAVEEDITEDCOMMENT";
-const DELETEPOST = "DELETEPOST";
 const DELETECOMMENT = "DELETECOMMENT"
 
 
-const postReducer = (state = {}, action) => {
+const commentReducer = (state = [], action) => {
 
     // whole state is passed in, the actions are very flat, meaning the payload is not encapsulated into its own property
 
-    let payload = action.payload;
+    const payload = action.payload;
 
     switch (action.type) {
-        case SAVENEWPOST || SAVENEWCOMMENT:
-            return {
-                ...state,
-                [payload.id]: {...payload}
+        case SAVENEWCOMMENT:
+            {
+                const deepCopyComments = state.map(
+                    comment => {
+                        return { ...comment }
+                    })
+                deepCopyComments.push(action.payload)
+                return deepCopyComments
             }
+        case DELETECOMMENT:
+            {
+                const deepCopyComments = state.map(comment => { return { ...comment } })
+                return deepCopyComments.filter(comment => comment.id !== action.id)
 
-
-        case DELETEPOST || DELETECOMMENT:
-            let newState = { ...state };
-            delete newState[action.id];
-            return newState
-
-        case SAVEEDITEDPOST:
-            return { ...store,
-                [payload.id] = {
-                    ...store[payload.id],
-                    title: payload.title,
-                    body: payload.body,
-                    author: payload.author,
-                    timestamp: payload.timestamp
-                }
             }
-
         case SAVEEDITEDCOMMENT:
-            return { ...state,
-                [payload.id] = {
-                    ...store[payload.id],
-                    body: payload.body,
-                    author: payload.author,
-                    timestamp: payload.timestamp
+            {
+                const editedComment = state.posts.filter(post => post.id === action.id);
+                const commentCopy = { ...editedComment };
+                commentCopy.body = payload.body;
+                commentCopy.author = payload.author;
+                commentCopy.timestamp = payload.timestamp;
+                otherComments = state.posts.filter(post => post.id !== action.id).map(post => { return { ...post } });
+                otherComments.push(commentCopy)
 
-                }
+                return otherComments
             }
-
-
-
-
         default:
             return state
     }
 }
 
 
-export default postReducer
+export default commentReducer
--- src/reducers/postReducer.js
+++ src/reducers/postReducer.js
@@ -6,36 +6,41 @@ const DELETEPOST = "DELETEPOST";
 
 const postReducer = (state = [], action) => {
 
-   // here we only want to pass in the posts property of the state
+    // here we only want to pass in the posts property of the state
 
     let payload = action.payload;
 
     switch (action.type) {
 
         case SAVENEWPOST:
-            // return array of new posts 
-            const deepCopyPosts = state.map(post=>{return{...post})
-            deepCopyPosts.push(action.payload)
-            return deepCopyPosts;
+            {
+                // return array of new posts 
+                const deepCopyPosts = state.map(post => { return { ...post } })
+                deepCopyPosts.push(action.payload)
+                return deepCopyPosts;
+            }
 
         case DELETEPOST:
-            const deepCopyPosts = state.map(post=>{return{...post})
-            return deepCopyPosts.filter(post=>post.id!==action.id)
-            
+            {
+                const deepCopyPosts = state.map(post => { return { ...post } })
+                return deepCopyPosts.filter(post => post.id !== action.id)
+            }
+
 
         case SAVEEDITEDPOST:
-            const editedPost = state.posts.filter(post=>post.id===action.id);
-            const postCopy = {...editedPost};
-            postCopy.title  = payload.title;
-            postCopy.body  = payload.body;
-            postCopy.author  = payload.author;
-            postCopy.timestamp  = payload.timestamp;
-            otherPosts = state.posts.filter(post=>post.id!==action.id).map(post=>{...post});
-            otherPosts.push(postCopy)
+            {
+                const editedPost = state.posts.filter(post => post.id === action.id);
+                const postCopy = { ...editedPost };
+                postCopy.title = payload.title;
+                postCopy.body = payload.body;
+                postCopy.author = payload.author;
+                postCopy.timestamp = payload.timestamp;
+                otherPosts = state.posts.filter(post => post.id !== action.id).map(post => { return { ...post } });
+                otherPosts.push(postCopy)
 
-            return otherPosts
-            
+                return otherPosts
 
+            }
 
 
 
