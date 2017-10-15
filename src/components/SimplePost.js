a9c09990f4c2413d39956839fc01217ba38f74f7
Ryan <rscharfer@gmail.com>
2017-10-15 13:06:34 +0200

    simple post is not yet a funcitonal component

--- src/components/SimplePost.js
+++ src/components/SimplePost.js
@@ -10,25 +10,25 @@ class SimplePost extends Component{
 
   constructor(props){
     super();
-    this.state={
-      commentTotal : 0,
-      voteTotal:props.meta.voteScore,
-    }
-    this.changeVoteTotal = this.changeVoteTotal.bind(this);
+    // this.state={
+    //   commentTotal : 0,
+    //   voteTotal:props.meta.voteScore,
+    // }
+    // this.changeVoteTotal = this.changeVoteTotal.bind(this);
   }
 
-  componentDidMount(){
+  // componentDidMount(){
 
-    const url = 'http://localhost:3001/posts/'+this.props.meta.id+'/comments';
-    const headers = {authorization:'crazypassword'};
-    fetch(url,{headers:headers}).then(resp=>resp.json()).then(json=>this.setState({commentTotal:json.length}));
+  //   const url = 'http://localhost:3001/posts/'+this.props.meta.id+'/comments';
+  //   const headers = {authorization:'crazypassword'};
+  //   fetch(url,{headers:headers}).then(resp=>resp.json()).then(json=>this.setState({commentTotal:json.length}));
 
-  }
+  // }
 
-  changeVoteTotal(direction){
-    if (direction==='up') this.setState((prevState,props)=>{voteTotal:prevState.voteTotal++})
-    else if (direction='down') this.setState((prevState,props)=>{voteTotal:prevState.voteTotal--})
-  }
+  // changeVoteTotal(direction){
+  //   if (direction==='up') this.setState((prevState,props)=>{voteTotal:prevState.voteTotal++})
+  //   else if (direction='down') this.setState((prevState,props)=>{voteTotal:prevState.voteTotal--})
+  // }
 
 
 
@@ -42,18 +42,18 @@ class SimplePost extends Component{
       <div className="columns is-centered">
       <div className="tile is-parent is-8">
             <article className="tile column is-child box">
-              <SimplePostTitle meta={this.props.meta}/>
-              <p className="subtitle">Total Votes: {this.state.voteTotal}</p>
+              <SimplePostTitle meta={this.props.post}/>
+              <p className="subtitle">Total Votes: {this.props.post.voteScore}</p>
             
              <nav className="breadcrumb" aria-label="breadcrumbs">
                 <ul>
-                <li><span>{this.props.meta.author}&nbsp;&nbsp;&nbsp;</span></li>
-                  <li><span>&nbsp;&nbsp;&nbsp;{this.props.meta.timeStamp}&nbsp;&nbsp;&nbsp;</span></li>
-                  <li><span>&nbsp;&nbsp;&nbsp;{this.state.commentTotal} Comments&nbsp;&nbsp;&nbsp;</span></li>
-                  <VoteUp meta={this.props.meta}/>
-                  <VoteDown meta={this.props.meta}/>
-                  <Edit meta={this.props.meta} showPopulatedForm={this.props.showPopulatedForm}/>
-                  <Delete meta={this.props.meta}/>                     
+                <li><span>{this.props.post.author}&nbsp;&nbsp;&nbsp;</span></li>
+                  <li><span>&nbsp;&nbsp;&nbsp;{this.props.post.timeStamp}&nbsp;&nbsp;&nbsp;</span></li>
+                  <li><span>&nbsp;&nbsp;&nbsp;{this.props.post.commentTotal} Comments&nbsp;&nbsp;&nbsp;</span></li>
+                  <VoteUp meta={this.props.post}/>
+                  <VoteDown meta={this.props.post}/>
+                  <Edit meta={this.props.post} showPopulatedForm={this.props.showPopulatedForm}/>
+                  <Delete meta={this.props.post}/>                     
                   
               </ul>
             </nav>
