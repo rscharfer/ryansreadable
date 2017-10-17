ef1772d1ba5e42f51fc27ffe6d94883d25e5b45e
Ryan <rscharfer@gmail.com>
2017-10-17 07:42:57 +0200

    detailed post also has the right time

--- src/components/DetailedPost.js
+++ src/components/DetailedPost.js
@@ -3,12 +3,14 @@ import VoteUp from './buttons/VoteUp.js'
 import VoteDown from './buttons/VoteDown.js'
 import Edit from './buttons/Edit.js'
 import Delete from './buttons/Delete.js'
+import dateFormat from 'dateformat'
 
 
 
 
 const DetailedPost = (props) =>{
 
+    const time = dateFormat(props.post.timestamp, "mmmm dS, yyyy")
 
     return (      
 
@@ -23,7 +25,7 @@ const DetailedPost = (props) =>{
               <nav className="breadcrumb" aria-label="breadcrumbs">
                 <ul>
                 <li><span>{props.post.author}&nbsp;&nbsp;&nbsp;</span></li>
-                  <li><span>&nbsp;&nbsp;&nbsp;Jan 5th, 2017&nbsp;&nbsp;&nbsp;</span></li>
+                  <li><span>&nbsp;&nbsp;&nbsp;{time}&nbsp;&nbsp;&nbsp;</span></li>
                   <li><span>&nbsp;&nbsp;&nbsp;{props.commentNumber} Comments&nbsp;&nbsp;&nbsp;</span></li>
                   <VoteUp meta={props.post} changeVote={this.changeVoteTotal}/>
                   <VoteDown meta={props.post} changeVote={this.changeVoteTotal}/>
