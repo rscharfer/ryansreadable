82a9686c19dacc31b29e27675e8ff1052ab2f959
Ryan <rscharfer@gmail.com>
2017-10-21 10:01:13 +0200

    cleaned up edit post

--- src/components/PostForm.js
+++ src/components/PostForm.js
@@ -26,8 +26,8 @@ class PostForm extends Component{
     completeStateForEditingActions(){
 
       const completeState = Object.assign({},this.state,{
-        id:props.postBeingEdited.id,
-        voteScore:props.postBeingEdited.voteScore
+        id:this.props.postBeingEdited.id,
+        voteScore:this.props.postBeingEdited.voteScore
       })
 
       return completeState;
@@ -83,7 +83,7 @@ class PostForm extends Component{
 
 <div className="field is-grouped">
   <div className="control">
-    <button className="button is-primary" onClick={this.props.postBeingEdited?()=>{this.props.saveEditedPost(completeStateForEditingActions())}:()=>{this.props.saveNewPost(this.state)}}>Save</button>
+    <button className="button is-primary" onClick={this.props.postBeingEdited?()=>{this.props.saveEditedPost(this.completeStateForEditingActions())}:()=>{this.props.saveNewPost(this.state)}}>Save</button>
   </div>
   <div className="control">
     <button className="button is-link" onClick={this.props.closePostForm}>Cancel</button>
