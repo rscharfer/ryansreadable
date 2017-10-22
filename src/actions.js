7ae7393579344ebcc61186f368d7a1e15342e939
Ryan <rscharfer@gmail.com>
2017-10-22 10:17:50 +0200

    add headers variable with token

--- src/actions.js
+++ src/actions.js
@@ -12,7 +12,11 @@ let token = localStorage.token
 if (!token)
    token = localStorage.token = Math.random().toString(36).substr(-8)
 
- 
+ const headers = {
+  'Accept': 'application/json',
+  'Authorization': token,
+  'Content-Type': 'application/json'
+}
 
 
 export function sortByDate() {
